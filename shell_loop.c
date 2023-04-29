34282a912457774cb5cb7413520f25c8b14b04c1
Qarlifa <udezuaemmanuel22@gmail.com>
2023-04-29 14:19:58 -0700

    update

--- shell_loop.c
+++ shell_loop.c
@@ -44,14 +44,15 @@ int hsh(info_t *info, char **av)
 }
 
 /**
- * find_builtin - finds a builtin @info: the parameter & return info struct
- * @* - a function that passes an argument when called
+ * find_builtin - finds a builtin command
+ * @info: the parameter & return info struct
+ *
  * Return: -1 if builtin not found,
- *0 if builtin executed successfully,
- *1 if builtin found but not successful,
- *-2 if builtin signals exit()
+ * 0 if builtin executed successfully,
+ * 1 if builtin found but not successful,
+ * -2 if builtin signals exit()
  */
-int find_builtin(info_t *)
+int find_builtin(info_t *info)
 {
 	int i, built_in_ret = -1;
 	builtin_table builtintbl[] = {
